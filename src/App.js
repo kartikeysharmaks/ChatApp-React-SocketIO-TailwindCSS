b45be2d938434c3e9e435a4844dd4e9ef49b833c
kartikeysharmaks <kartikeysharma1616@gmail.com>
2024-03-16 22:57:21 +0530

    updated env file

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import Home from './components/Home.jsx';
 import ChatPage from './pages/ChatPage.jsx';
 import socketIO from 'socket.io-client';
 
-const socket = socketIO.connect('http://localhost:4000');
+const url = process.env.PRODUCTION_URL ? process.env.PRODUCTION_URL : 'http://localhost:4000';
+const socket = socketIO.connect(url);
 function App() {
   return (
     <BrowserRouter>
